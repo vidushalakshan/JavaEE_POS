0c2d5c3f27f970a85321af0021c99e414938585c
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-06-17 12:07:53 +0530

    updated itemBO

--- JavaEE_BackEnd/src/bo/custom/itemBO.java
+++ JavaEE_BackEnd/src/bo/custom/itemBO.java
@@ -1,5 +1,25 @@
 package bo.custom;
 
-public interface itemBO {
+import dto.itemDTO;
 
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import java.sql.SQLException;
+
+public interface itemBO {
+    JsonArrayBuilder getAllItems() throws SQLException;
+
+    JsonObjectBuilder generateItemID() throws SQLException;
+
+    JsonArrayBuilder searchItem(String id) throws SQLException;
+
+    boolean addItem(itemDTO itemDTO) throws SQLException;
+
+    boolean deleteItem(String id) throws SQLException;
+
+    boolean updateItem(itemDTO itemDTO) throws SQLException;
+
+    JsonArrayBuilder loadAllItemIDs() throws SQLException;
+
+    JsonArrayBuilder loadSelectedItemData(String id) throws SQLException;
 }
