e96c01f49bf19aa5cde2b5934136aa2ed9027c78
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-06-17 12:04:21 +0530

    updated customerBO

--- JavaEE_BackEnd/src/entity/customer.java
+++ JavaEE_BackEnd/src/entity/customer.java
@@ -1,4 +1,60 @@
 package entity;
 
 public class customer {
+    private String id;
+    private String name;
+    private String address;
+    private int salary;
+
+    public customer() {
+    }
+
+    public customer(String id, String name, String address, int salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
